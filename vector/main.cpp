3521ff16f29fef512b3da1f49278794831652afa
ArpineOhanyan <arpineohanyan3@gmail.com>
2021-04-02 19:29:40 +0400

    in our style

--- vector/main.cpp
+++ vector/main.cpp
@@ -2,18 +2,18 @@
 
 int main ()
 {
-    vector<int> arr(4,5);
-    arr.push_back(9);
-    vector<int> a = arr;
-    for(size_t i = 0; i < a.size(); ++i) {
-        std::cout<<a[i]<<" ";
+    Vector<int> array (4,5);
+    array.PushBack (9);
+    Vector<int> arr = array;
+    for (size_t i = 0; i < arr.Size(); ++i) {
+        std::cout << arr[i] <<" ";
     } std::cout << std::endl;
-//  a.shrink_to_fit();
-    a.reserve(3);
-    std::cout <<a.capacity() << std::endl;
-    std::cout <<a.size() << std::endl;
-     for(size_t i = 0; i < a.size(); ++i) {
-        std::cout<<a[i]<<" ";
+//  array.ShrinkToFit();
+    arr.Reserve (3);
+    std::cout << arr.Capacity() << std::endl;
+    std::cout << arr.Size() << std::endl;
+     for (size_t i = 0; i < arr.Size(); ++i) {
+        std::cout << arr[i] << " ";
     } std::cout << std::endl;
     return 0;
 }
