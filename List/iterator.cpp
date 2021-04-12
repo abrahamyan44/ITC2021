2a023959267761673d9ccc2de7b89a65f779308c
ArpineOhanyan <arpineohanyan3@gmail.com>
2021-04-12 08:20:38 +0400

    our coding style

--- List/iterator.cpp
+++ List/iterator.cpp
@@ -12,30 +12,30 @@ Iterator::Iterator (const Iterator& itr)
     :iterator (itr.iterator) {}
 
 void Iterator::Next() {
-	if (iterator == nullptr) {
-		return;
-	} else {
-		iterator =  iterator->m_next;
-	}
+    if (iterator == nullptr) {
+        return;
+    } else {
+        iterator =  iterator->m_next;
+    }
 }
 
 void Iterator::Prev() {
-	if (iterator == nullptr) {
+    if (iterator == nullptr) {
         return;
-	} else {
-		iterator =  iterator->m_prev;
-	}
+    } else {
+        iterator =  iterator->m_prev;
+    }
 }
 
 Iterator& Iterator::operator++() {
-	Next();
-   	return *this;
+    Next();
+    return *this;
 }
 
 Iterator Iterator::operator++ (int) {
-	auto current(*this);
-	operator++();
-	return current;
+    auto current(*this);
+    operator++();
+    return current;
 }
 
 Iterator& Iterator::operator--() {	
@@ -44,23 +44,23 @@ Iterator& Iterator::operator--() {
 }
 
 Iterator Iterator::operator-- (int) {
-	auto temp (*this);
-	operator-- ();
-	return temp;
+    auto temp (*this);
+    operator-- ();
+    return temp;
 } 
 
 bool Iterator::operator== (const Iterator& itr) {
-	return iterator == itr.iterator;
+    return iterator == itr.iterator;
 } 
 
 bool Iterator::operator!= (const Iterator& itr) {
-	return iterator != itr.iterator;
+    return iterator != itr.iterator;
 } 
 
 int& Iterator::operator*() {
-	return iterator->m_data;
+    return iterator->m_data;
 }
 
 Iterator& Iterator::operator->() {
-	return *this;
+    return *this;
 }
