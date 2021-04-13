f065e2a84e979bd633e7bead20bb2a4d52418b96
anna_ha <annnahakopyan@gmail.com>
2021-04-13 11:02:29 +0400

    added Shell-sort algorithm v1.0.0

---
+++ sorts/shell_sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+void ShellSort(const int& n, int* a)
+{
+	for(int interval = n / 2; interval > 0; interval /= 2)
+	{
+		for(int i = interval; i < n; ++i)
+		{
+			for(int j = i; j >= interval; j -= interval)
+			{
+				if(a[j] < a[j - interval])
+				{
+					swap(a[j], a[j - interval]);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for(int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+	}
+	ShellSort(n, a);
+	for(int i = 0; i < n; ++i)
+	{
+		cout << a[i] << " ";
+	}
+	delete[] a;
+	return 0;
+}
