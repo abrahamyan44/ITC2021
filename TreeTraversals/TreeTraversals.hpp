cfcad792e84cc7985f363ec3e83a5a865982f9c7
heghine11 <79168368+heghine11@users.noreply.github.com>
2021-04-14 14:56:24 +0400

    Update TreeTraversals.hpp

--- TreeTraversals/TreeTraversals.hpp
+++ TreeTraversals/TreeTraversals.hpp
@@ -1,8 +1,8 @@
 template <class T>
-struct Node 
+struct node 
 {
-    Node <T>* left;
-    Node <T>* right;
+    node <T>* left;
+    node <T>* right;
     T data;
 };
 
@@ -10,19 +10,19 @@ template <class T>
 class Tree
 {
 private:
-    Node <T>* m_root;
+    node <T>* m_root;
 
 public:
     Tree(); //empty tree with no nodes
-    Node <T>* CreateNode(T data); //creating new node
+    node <T>* CreateNode(T data); //creating new node
     ~Tree(); 
 
-    void MakeEmpty(Node <T>* leaf); //function for helping destructor
+    void MakeEmpty(node <T>* leaf); //function for helping destructor
     
-    void InOrderRecursively(Node <T>* root); //recursive function for inorder tree traversal
-    void PreOrderRecursively(Node <T>* root); 
-    void PostOrderRecursively(Node <T>* root);  
+    void InOrderRecursively(node <T>* root); //recursive function for inorder tree traversal
+    void PreOrderRecursively(node <T>* root); 
+    void PostOrderRecursively(node <T>* root);  
 
-    void InOrder(Node <T>* root); //iterative function for inorder tree traversal
-    void Print(Node <T>* root); //function for printing the tree level by level
+    void InOrder(node <T>* root); //iterative function for inorder tree traversal
+    void Print(node <T>* root); //function for printing the tree level by level
 };
