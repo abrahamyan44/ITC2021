5583cfdf0a322f115a0b078a1aa88ab642022514
heghine11 <79168368+heghine11@users.noreply.github.com>
2021-04-14 14:50:18 +0400

    Update StackClass.cpp

--- StackClass/StackClass.cpp
+++ StackClass/StackClass.cpp
@@ -3,7 +3,7 @@
 #include "StackClass.hpp"
 
 template <class T>
-Stack <T>::Stack() : m_top(0), m_size(0)
+Stack <T>::Stack () : m_top(0), m_size(0)
 {}
 
 template <class T>
@@ -11,102 +11,102 @@ Stack <T>::Stack (const T& value, int count) : m_top(0), m_size(0)
 {
     for (int i=0; i < count; i++)
     {
-        Node <T>* temp = new Node <T>(value, m_top);
-        m_top = temp;
+        node <T>* temporal = new node <T>(value, m_top);
+        m_top = temporal;
         m_size++;
     }
 }
 
 template <class T>
-Stack <T>::Stack(const Stack <T>& x) : m_top(0), m_size(x.m_size)
+Stack <T>::Stack (const Stack <T>& other_stack) : m_top(0), m_size(other_stack.m_size)
 {
-    Node <T>* current = x.m_top;
-    m_top = new Node <T>;
+    node <T>* current = other_stack.m_top;
+    m_top = new node <T>;
     m_top->data = current->data;
     m_top->link = 0;
 
-    Node <T>* last = m_top;
+    node <T>* last = m_top;
     current = current->link;
     while (current != 0)
     {
-        Node <T>* temp = new Node <T>;
-        temp->data = current->data;
-        temp->link = 0;
-        last->link = temp;
-        last = temp;
+        node <T>* temporal = new node <T>;
+        temporal->data = current->data;
+        temporal->link = 0;
+        last->link = temporal;
+        last = temporal;
         current = current->link;
     }
 }
 
 template <class T>
-Stack <T>::~Stack()
+Stack <T>::~Stack ()
 {
     std::cout << "Destructed!" << std::endl;
-    Node <T>* temp = m_top;
-    while (temp->link != 0)
+    node <T>* temporal = m_top;
+    while (temporal->link != 0)
     {
-        temp = temp->link;
+        temporal = temporal->link;
         delete m_top;
-        m_top = temp;
+        m_top = temporal;
     }
     m_size = 0;
 }
 
 template <class T>
-void Stack <T>::Pop()
+void Stack <T>::Pop ()
 {
     if (!IsEmpty())
     {
-        Node <T>* temp = m_top->link;
+        node <T>* temporal = m_top->link;
         delete m_top;
-        m_top = temp;
+        m_top = temporal;
         m_size--;
     }
     else
     {
-            std::cout << "Nothing to pop!" << std::endl;
+        std::cout << "Nothing to pop!" << std::endl;
     }
 }
 
 template <class T>
-void Stack <T>::Push(const T& value)
+void Stack <T>::Push (const T& value)
 {
-    Node <T>* temp = new Node <T>;
+    node <T>* temporal = new node <T>;
     if (IsEmpty())
     {
-        temp->data = value;
-        m_top = temp;
+        temporal->data = value;
+        m_top = temporal;
     }
     else
     {
-        temp->data = value;
-        temp->link = m_top;
-        m_top = temp;
+        temporal->data = value;
+        temporal->link = m_top;
+        m_top = temporal;
     }
     m_size++;
 }
 
 template <class T>
-int Stack <T>::GetSize() const
+int Stack <T>::GetSize () const
 {
     return m_size;
 }
 
 template <class T>
-bool Stack <T>::IsEmpty() const
+bool Stack <T>::IsEmpty () const
 {
     return (m_size == 0);
 }
 
 template <class T>
-T& Stack <T>::GetTop()
+T& Stack <T>::GetTop ()
 {
         assert(m_top != 0);
         return m_top->data;
 }
 
 template <class T>
-void Stack <T>::Print()
+void Stack <T>::Print ()
 {
     if (IsEmpty())
     {
@@ -114,11 +114,11 @@ void Stack <T>::Print()
     }
     else
     {
-        Node <T>* temp = m_top;
-        while (temp)
+        node <T>* temporal = m_top;
+        while (temporal)
         {
-            std::cout << temp->data << " ";
-            temp = temp->link;
+            std::cout << temporal->data << " ";
+            temporal = temporal->link;
         }
     }
     std::cout << std::endl;
@@ -127,37 +127,36 @@ void Stack <T>::Print()
 
 int main()
 {
-    Stack <int> s1;
-    s1.Print(); 
-    Stack <int> s2(9, 4);
-    std::cout << "s2's elements are: " << std::endl;
-    s2.Print();
-    Stack <int> s3(s2);
-    std::cout << "s3's elements are the copy of s2: " << std::endl;
-    s3.Print(); 
-    s1.Push(1);
-    s1.Push(2);
-    s1.Push(3);
-    s1.Push(4);
-    std::cout << "s1's elements after pushing them: " << std::endl;
-    s1.Print();
-    Stack <int> s4(s1);
-    std::cout << "s4's elements are the copy of s1: " << std::endl;
-    s4.Print();
-    s1.Pop();
-    std::cout << "s1's elements after poping: " << std::endl;
-    s1.Print();
-    s2.Pop();
-    std::cout << "s2's elements after poping: " <<std::endl;
-    s2.Print();
-    std::cout << "s1's size is: " << s1.GetSize() << std::endl;
-    std::cout << "s1's top is: " << s1.GetTop() << std::endl;
+    Stack <int> stack1;
+    stack1.Print(); 
+    Stack <int> stack2(9, 4);
+    std::cout << "stack2's elements are: " << std::endl;
+    stack2.Print();
+    Stack <int> stack3(stack2);
+    std::cout << "stack3's elements are the copy of stack2: " << std::endl;
+    stack3.Print(); 
+    stack1.Push(1);
+    stack1.Push(2);
+    stack1.Push(3);
+    stack1.Push(4);
+    std::cout << "stack1's elements after pushing them: " << std::endl;
+    stack1.Print();
+    Stack <int> stack4(stack1);
+    std::cout << "stack4's elements are the copy of stack1: " << std::endl;
+    stack4.Print();
+    stack1.Pop();
+    std::cout << "stack1's elements after poping: " << std::endl;
+    stack1.Print();
+    stack2.Pop();
+    std::cout << "stack2's elements after poping: " <<std::endl;
+    stack2.Print();
+    std::cout << "stack1's size is: " << stack1.GetSize() << std::endl;
+    std::cout << "stack1's top is: " << stack1.GetTop() << std::endl;
     
-    if (!s1.IsEmpty())
+    if (!stack1.IsEmpty())
     {
-        std::cout << "s1 is not empty." << std::endl;
+        std::cout << "stack1 is not empty." << std::endl;
     }
     
     return 0;
 }
-
