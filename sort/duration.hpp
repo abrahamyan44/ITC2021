9738fddc946a974a9fc425bc23d380326b363e97
ArpineOhanyan <arpineohanyan3@gmail.com>
2021-04-20 00:46:47 +0400

    sorting algorithms

---
+++ sort/duration.hpp
@@ -0,0 +1,63 @@
+#include "sorting.hpp"
+
+namespace duration
+{
+    template <class T>
+    T* CopyArray(T* array, int n) {
+        T* new_array = new int[n];
+        for(int i = 0; i < n; ++i) {
+            new_array[i] = array[i];
+        }
+        return new_array;
+    }
+
+    template <class T>
+    bool SortVerification(T* array, int n)
+    {
+        bool is_sorted;
+        for(int i = 0; i < n - 1; ++i) {
+            is_sorted = false;
+            if(array[i] <= array[i + 1]) {
+                is_sorted = true;
+            }
+        }
+        return is_sorted;
+    }
+
+	template <class T>
+    long double Duration(void (*func_name)(T*, int, int), T* array, int n)
+    {
+        T* new_array = CopyArray(array, n);
+        auto start = std::chrono::system_clock::now();
+        (*func_name)(new_array, 0, n - 1);
+        auto stop = std::chrono::system_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        if(SortVerification(new_array, n)) {
+            return duration;
+        }
+        else {
+            return -1;
+        }
+        delete [] new_array;
+    }
+
+	template <class T>
+    long double Duration(void (*func_name)(T*, int), T* array, int n)
+    {
+        T* new_array = CopyArray(array, n);
+        auto start = std::chrono::system_clock::now();
+        (*func_name)(new_array, n);
+        auto stop = std::chrono::system_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        if(SortVerification(new_array, n)) {
+            return duration;
+        }
+        else {
+            return -1;
+        }
+        delete [] new_array;
+    }
+}
+
+
+
---
+++ sort/main.cpp
@@ -0,0 +1,20 @@
+#include "testing.hpp"
+
+int main(int argc, char* argv[])
+{
+	int size = std::stoi(argv[1]);
+	int test_count = std::stoi(argv[2]);
+	int* array = nullptr;
+    assert(size > 0);
+    array = new int64_t[size];
+    srand(time(0));
+    for(int i = 0; i < size; ++i) {
+        array[i] = rand() % 3425;
+    }
+		test::TestingQuick(array, size, test_count);
+		test::TestingHeap(array, size, test_count);
+		test::TestingMerge(array, size, test_count);
+		test::TestingBubble(array, size, test_count);
+		test::TestingShell(array, size, test_count);
+	return 0;
+}
