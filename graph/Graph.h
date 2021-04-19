b47438fbeca27599978ab54ab3c4a8c9750ef516
SEPUH H <sepuh.hovhannisyan.01@mail.ru>
2021-04-19 22:00:53 +0400

    Added Dijktras algorithm implementation and edge weight

--- graph/Graph.h
+++ graph/Graph.h
@@ -1,57 +1,62 @@
 #pragma once
 #include <iostream>
 #include <queue>
+#include <set>
 
 class Graph
 {
 private:
-    bool** m_vertice_matrix;
+    int** m_vertice_matrix;
     int m_vertices_count;
 
-    bool* CreateTableForShowPassedVertices ();
-    void ImplementDFSRecursive (int vertice, bool* is_vertice_passed);
+    bool* CreateTableForShowPassedVertices();
+    void ImplementDFSRecursive(int vertice, bool* is_vertice_passed);
+    void ImplementDijktrasAlgorithmRecursive(int vertice, std::set<int>& visited, std::pair<int, int>* info);
+    bool IsVerticeVisited(int vertice, std::set<int>& visited);
 
 public:
-    Graph (int count);
+    Graph(int count);
 
-    ~Graph ();
+    ~Graph();
 
-    void AddingEdge (int v, int w);
+    void AddingEdge(int v, int w, int weight=1);
 
-    void ImplementBFS (int vertice);
+    void ImplementBFS(int vertice);
 
-    void ImplementDFS (int vertice);
+    void ImplementDFS(int vertice);
+
+    void ImplementDijktrasAlgorithm(int vertice);
 };
 
 
-Graph::Graph (int count) 
+Graph::Graph(int count) 
     :m_vertices_count(count) {
-    m_vertice_matrix = new bool*[m_vertices_count];
+    m_vertice_matrix = new int*[m_vertices_count];
     for (int i = 0; i < m_vertices_count; ++i) {
-        m_vertice_matrix[i] = new bool[m_vertices_count];
+        m_vertice_matrix[i] = new int[m_vertices_count];
         for (int j = 0; j < m_vertices_count; ++j) {
             m_vertice_matrix[i][j] = false;
         }
     }
 }
 
-Graph::~Graph () {
+Graph::~Graph() {
     for(int i = 0; i < m_vertices_count; ++i) {
         delete[] m_vertice_matrix[i];
     }
     delete[] m_vertice_matrix;
 }
 
-void Graph::AddingEdge (int v, int w) {
+void Graph::AddingEdge(int v, int w, int weight) {
     if (v < m_vertices_count && v >= 0 && w < m_vertices_count && w >= 0) {
-        m_vertice_matrix[v][w] = true;
-        m_vertice_matrix[w][v] = true;
+        m_vertice_matrix[v][w] = weight;
+        m_vertice_matrix[w][v] = weight;
     }
 
     return;
 }
 
-bool* Graph::CreateTableForShowPassedVertices () {
+bool* Graph::CreateTableForShowPassedVertices() {
     bool* table = new bool[m_vertices_count];
     for (int i = 0; i < m_vertices_count; ++i) {
         table[i] = 0;
@@ -59,7 +64,7 @@ bool* Graph::CreateTableForShowPassedVertices () {
     return table;
 }
 
-void Graph::ImplementBFS (int vertice) {
+void Graph::ImplementBFS(int vertice) {
     if (vertice < 0 || vertice >= m_vertices_count) {
         return;
     }
@@ -70,7 +75,7 @@ void Graph::ImplementBFS (int vertice) {
     while (!q.empty()) {
         vertice = q.front();
         for (int i = 0; i < m_vertices_count; ++i) {
-            if (m_vertice_matrix[vertice][i] == true && is_vertice_passed[i] == false) {
+            if (m_vertice_matrix[vertice][i] > 0 && is_vertice_passed[i] == false) {
                 q.push(i);
                 is_vertice_passed[i] = true;
             }
@@ -81,7 +86,7 @@ void Graph::ImplementBFS (int vertice) {
     std::cout << "\n";
 }
 
-void Graph::ImplementDFS (int vertice) {
+void Graph::ImplementDFS(int vertice) {
     if (vertice < 0 || vertice >= m_vertices_count) {
         return;
     }
@@ -90,12 +95,57 @@ void Graph::ImplementDFS (int vertice) {
     std::cout << "\n";
 }
 
-void Graph::ImplementDFSRecursive (int vertice, bool* is_vertice_passed) {
+void Graph::ImplementDFSRecursive(int vertice, bool* is_vertice_passed) {
     is_vertice_passed[vertice] = true;
     std::cout << vertice << " ";
     for (int i = 0; i < m_vertices_count; ++i) {
-        if (m_vertice_matrix[vertice][i] == true && is_vertice_passed[i] == false) {
+        if (m_vertice_matrix[vertice][i] > 0 && is_vertice_passed[i] == false) {
             ImplementDFSRecursive(i, is_vertice_passed);
         }
     }
+}
+
+void Graph::ImplementDijktrasAlgorithm(int vertice) {
+    std::set<int> visited;
+    std::pair<int, int>* info_table = new std::pair<int, int>[m_vertices_count];
+    for (int i = 0; i < m_vertices_count; ++i) {
+        info_table[i].first = -1;
+        info_table[i].second = -1;
+    }
+    info_table[vertice].first = 0;
+    ImplementDijktrasAlgorithmRecursive(vertice, visited, info_table);
+    for (int i = 0; i < m_vertices_count; ++i) {
+        std::cout << i << " " << info_table[i].first << " " << info_table[i].second << std::endl;
+    }
+}
+
+void Graph::ImplementDijktrasAlgorithmRecursive(int vertice, std::set<int>& visited, std::pair<int, int>* info) {
+    visited.insert(vertice);
+    for (int i = 0; i < m_vertices_count; ++i) {
+        if (m_vertice_matrix[vertice][i] > 0 && (info[i].first == -1 || m_vertice_matrix[vertice][i] + info[vertice].first < info[i].first)) {
+            info[i].first = m_vertice_matrix[vertice][i] + info[vertice].first;
+            info[i].second = vertice;
+        }
+    }
+    
+    while (1) {
+        int min_value_index = -1;
+        for (int i = 0; i < m_vertices_count; ++i) {
+            if (info[i].first != -1 && !IsVerticeVisited(i, visited) && (min_value_index == -1 || info[i].first < info[min_value_index].first)) {
+                min_value_index = i;
+            }
+        }
+        if (min_value_index == -1) {
+            break;
+        }
+        ImplementDijktrasAlgorithmRecursive(min_value_index, visited, info);
+    }
+}
+
+bool Graph::IsVerticeVisited(int vertice, std::set<int>& visited) {
+    auto it = visited.find(vertice);
+    if (it != visited.end()) {
+        return true;
+    }
+    return false;
 }
--- graph/main.cpp
+++ graph/main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 int main () {
     Graph G(5);
-    G.AddingEdge(0, 1);
-    G.AddingEdge(0, 2);
-    G.AddingEdge(4, 0);
-    G.AddingEdge(3, 1);
-    G.ImplementBFS(2);
-    G.ImplementDFS(0);
+    G.AddingEdge(0, 1, 15);
+    G.AddingEdge(1, 2, 10);
+    G.AddingEdge(3, 2, 3);
+    G.AddingEdge(4, 3, 3);
+    G.AddingEdge(3, 1, 2);
+    G.AddingEdge(3, 0, 4);
+    G.AddingEdge(2, 0, 5);
+    G.AddingEdge(4, 1, 3);
+    G.ImplementDijktrasAlgorithm(0);
     return 0;
 }
