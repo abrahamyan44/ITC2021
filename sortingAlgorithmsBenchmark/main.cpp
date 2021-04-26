824b73aff31e3b1c5fd72095f94d1bd0ce11031d
Yeva <eva.hovnanyan.1999@gmail.com>
2021-04-26 23:47:02 +0400

    Added array of function pointers.

--- sortingAlgorithmsBenchmark/main.cpp
+++ sortingAlgorithmsBenchmark/main.cpp
@@ -6,35 +6,32 @@
 
 using namespace Sortings;
 
-void TestsCall(int argc, char* argv[])
-{
+typedef void (*f_t1)(int*, int); 		// function type1
+typedef void (*f_t2)(int*, int, int);   // function type2
+
+void TestsCall(int argc, char* argv[]) {
 	int size = atoi(argv[1]);
 	int tests_count = atoi(argv[2]);	
 	int* array = RandomArrayGenerator(size);
+	std::string function_name[] = { "BubbleSort   ", "InsertionSort", "SelectionSort",
+								    "ShellSort    ", "HeapSort     ", "STLQuickSort ",
+								    "QuickSort    ", "MergeSort    " };
 
-	std::cout << std::endl << "Runing time of the BubbleSort:    ";
-	Testing1(array, size, BubbleSort, tests_count);
+	f_t1 array_of_sorts1[]={ BubbleSort, InsertionSort, SelectionSort,
+							ShellSort, HeapSort, STLQuickSort };
 	
-	std::cout << std::endl << "Runing time of the SelectionSort: ";
-	Testing1(array, size, SelectionSort, tests_count);
-	
-	std::cout << std::endl << "Runing time of the InsertionSort: ";
-	Testing1(array, size, InsertionSort, tests_count);
-	
-	std::cout << std::endl << "Runing time of the HeapSort:      ";
-	Testing1(array, size, HeapSort, tests_count);
-	
-	std::cout << std::endl << "Runing time of the ShellSort:     ";
-	Testing1(array, size, ShellSort, tests_count);
-	
-	std::cout << std::endl << "Runing time of the MergeSort:     ";
-	Testing2(array, 0, size - 1, MergeSort, tests_count);	
+	f_t2 array_of_sorts2[] = { QuickSort , MergeSort }; 
 
-	std::cout << std::endl << "Runing time of the QuickSort:     ";
-	Testing2(array, 0, size - 1, QuickSort, tests_count);
-
-	std::cout << std::endl << "Runing time of the STL's qsort:   ";	
-	Testing1(array, size, STLQuickSort, tests_count);	
+	for (int i = 0, j = 0; i < 8; ++i) {
+		if (i < 6) {	
+			std::cout << function_name[i] << "  ";		
+			Testing1(array, size, array_of_sorts1[i], tests_count);
+		} else {
+			std::cout << function_name[i] << "  ";		
+			Testing2(array, 0, size - 1, array_of_sorts2[j], tests_count);
+			++j;
+		}
+	}
 }
 
 int main(int argc, char* argv[])
