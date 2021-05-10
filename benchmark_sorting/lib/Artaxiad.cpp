c67e68c1a3651bd6442af3e1b822a705ee6cffdd
SEPUH H <sepuh.hovhannisyan.01@mail.ru>
2021-05-10 11:04:02 +0400

    Fixed all

--- benchmark_sorting/lib/Artaxiad.cpp
+++ benchmark_sorting/lib/Artaxiad.cpp
@@ -21,7 +21,7 @@ void Swap(int* a, int* b) {
 void BubbleSortByArtaxiad(int size, int* array) {
     bool swapped = false;
     for (int i = 0; i < size; ++i) {
-		for (int j = 0; j < size - i; ++j) {
+		for (int j = 0; j < size - i - 1; ++j) {
 			if (array[j] > array[j + 1]) {
 				Swap(&array[j], &array[j + 1]);
 				swapped = true;
