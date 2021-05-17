b4e58fae78917a8ddbe72d3423e60f2d8b0c8f9e
HakobyAnna <annnahakopyan@gmail.com>
2021-05-17 13:37:21 +0400

    Shape_Class/

---
+++ Web/Shape_Class/shape.js
@@ -0,0 +1,26 @@
+class Shape {
+    constructer() {
+
+    }
+    let print () {
+
+    }
+}
+
+class Rectangle extends Shape {
+    constructer () {
+
+    }
+}
+
+class Triangle  extends Shape {
+    constructer () {
+
+    }
+}
+
+class Circle  extends Shape {
+    constructer () {
+
+    }
+}
