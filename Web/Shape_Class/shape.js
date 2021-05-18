8ede7e7850968f3e9b24c6566d6360be9be8a9e8
HakobyAnna <annnahakopyan@gmail.com>
2021-05-18 13:34:45 +0400

    Task_Shape

--- Web/Shape_Class/shape.js
+++ Web/Shape_Class/shape.js
@@ -2,25 +2,50 @@ class Shape {
     constructer() {
 
     }
-    let print () {
-
-    }
+	// GetArea = function() {};
+	print() {
+		console.log( this.GetArea() ); 
+	}
 }
+
 
 class Rectangle extends Shape {
-    constructer () {
-
+    constructer (width, lenght) {
+        super();
+		this.width = width;
+		this.lenght = lenght;
     }
+	
+	GetArea() {
+			let area =  this.width * this.lenght;
+			return area;
+	}
 }
+
 
 class Triangle  extends Shape {
-    constructer () {
-
+    constructer (base, height) {
+		this.base = base;
+		this.height = height;
     }
+
+	GetArea() {
+		let area = this.base * this.heigt / 2;
+		return area;
+	}
 }
+
 
 class Circle  extends Shape {
-    constructer () {
-
+    constructer (radius) {
+		this.radius = radius;
     }
+		
+	GetArea() {
+		let area = math.pi * this.radius * this.radius;
+		return area;
+	}
 }
+
+let r = new Rectangle(4, 6);
+r.print();
