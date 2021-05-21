948c5cf9aeadfece67347ef030653ea9dfe9ca74
HakobyAnna <annnahakopyan@gmail.com>
2021-05-21 15:07:59 +0400

    adding functions

--- test/src/book.js
+++ test/src/book.js
@@ -1,22 +1,36 @@
-// data = [
-//     {"author": "Yuval Noah" , "title": "A History of Tomorrow", "path": "https://newmag.am/public/home/assets/img/books/thumbs/2cc9ad900e3e0df25f20cc9b652a5889.jpg"},
-//     {"author": "Yuval Noah" , "title": "A History of Tomorrow", "path": "https://newmag.am/public/home/assets/img/books/thumbs/2cc9ad900e3e0df25f20cc9b652a5889.jpg"}
-// ]
-const data = require('../src/books.json')
-// import data from '../src/books.json';
-window.onload = function() {
+const data = require('./books.json')
 console.log('aaa')
 let count_form = document.querySelector('.block_container');
 console.log(count_form);
 count_form.addEventListener('submit', function(event) {
     event.preventDefault();
+    document.querySelector(".container").innerHTML = '';
     let input_tag = document.querySelector('#count').value;
-    console.log(input_tag);
     let parent = document.querySelector(".container");
     console.log(parent);
     for(let i = 0; i < input_tag; i++) {
         let card = document.createElement('div');
-        card.textContent = data[i].author;
-        parent.appendChild(card);
-    }
-})}
+        let author = document.createElement('p');
+        author.textContent = data[i].author;
+        card.appendChild(author);
+        let title = document.createElement('p');
+        title.textContent = data[i].title;
+        card.appendChild(title);
+        let image = document.createElement('img');
+        image.src = data[i].img;
+        card.appendChild(image);
+        let bookButton = document.createElement('span');
+        bookButton.className = 'book_button';
+        let buy = document.createElement('button');
+        buy.textContent = "Buy";
+        bookButton.appendChild(buy);
+        let Delete = document.createElement('button');
+        Delete.textContent = "Delete";
+        bookButton.appendChild(Delete);
+        container.addEventListener('click', function(event) {
+            card.appendChild(bookButton);
+            card.className = 'col-md';
+            parent.appendChild(card);
+        }
+    document.querySelector('#count').value = '';
+}})
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: "development",
-  entry: './src/index.js',
+  entry: './src/book.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
