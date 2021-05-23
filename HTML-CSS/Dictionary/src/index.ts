f33f240e193a0e514fcf54c86b397c09d28bfe95
KhachatryaVahe <vahe.khachatryan.1999@gmail.com>
2021-05-23 10:13:04 -0400

    Commit 20: Has changed Dictionary project files.

--- HTML-CSS/Dictionary/src/index.ts
+++ HTML-CSS/Dictionary/src/index.ts
@@ -1,51 +1,60 @@
 import * as _ from 'lodash';
 import books from './books.json';
 
-// console.log(books.length);
-
-const list = document.createElement('div');
+var list = document.createElement('div');
 list.className = 'list-books';
 
-for (let index = 0; index < books.length; index++) {
-  console.log(index);
-
-  const element = books[index];
+function main() {
+  const formBlock = <HTMLInputElement>document.querySelector('#form-block')
   
-  const title = document.createElement('div');
-  title.textContent = element.title;
-  title.className = 'title';
+  formBlock.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    let inputValue: number = parseInt((<HTMLInputElement>document.querySelector('#input-block')).value);  
+    if (0 < inputValue && inputValue < books.length) {
+      // removeBook();
+      setBooks(inputValue);
+      // removeBook();
+      console.log('list = ', list);
 
-  const author = document.createElement('div');
-  author.textContent = element.author;
-  author.className = 'author';
-
-  const image = document.createElement('img');
-  image.src = element.image;
-  image.className = 'image';
-  
-  const book = document.createElement('div');
-  book.className = 'book';
-  book.appendChild(title);
-  book.appendChild(author);
-  book.appendChild(image);
-
-  list.appendChild(book);
+    } else {
+      alert("Wrong input value.");
+    }
+  });
 }
 
-document.querySelector('.list-books').appendChild(list)
+function setBooks(count:number) {  
+  for (let index = 0; index < count; index++) {
+    
+    const element = books[index];
+    
+    const title = document.createElement('div');
+    title.textContent = element.title;
+    title.className = 'title';
+    
+    const author = document.createElement('div');
+    author.textContent = element.author;
+    author.className = 'author';
+    
+    const image = document.createElement('img');
+    image.src = element.image;
+    image.className = 'image';
+    
+    const book = document.createElement('div');
+    book.className = 'book';
+    
+    book.appendChild(image);
+    book.appendChild(title);
+    book.appendChild(author);
+    list.appendChild(book);
+  }  
+  document.querySelector('body').appendChild(list);
+}
 
+function removeBook() {
+  // console.log(list);
+  if (list) {
+    list.parentElement.removeChild(list);
+  }
+}
 
-
-
-
-
-// function component() {
-//     const element = document.createElement('div');
-  
-//     // Lodash, currently included via a script, is required for this line to work
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-//     return element;
-//   }
-  
-  // document.body.appendChild(component());
+main();
