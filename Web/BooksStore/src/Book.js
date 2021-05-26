3e229156fa4fd52db71a990a15ad3931dac64e0e
yeva9 <eva.hovnanyan.1999@gmail.com>
2021-05-26 15:47:16 +0400

    Added delete buttons.

--- Web/BooksStore/src/Book.js
+++ Web/BooksStore/src/Book.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
-
-import data from '../books.json'
+import data from './books.json'
 
 const count_form = document.querySelector('#count-form');
 
@@ -8,42 +7,32 @@ count_form.addEventListener('submit', function (event) {
     event.preventDefault();
     const input_tag = document.querySelector('#input-tag').value;
     const parent = document.querySelector(".parent");
-    parent.innerHTML = " ";
+    parent.innerHTML = "";
+
     for (let i = 0; i < input_tag; i++) {
         const card = document.createElement('div');
         const image = document.createElement('img');
         const author = document.createElement('p');
         const title = document.createElement('p');
+        const deleteButton = document.createElement('button');
 
         card.className = 'book-item';
         image.className = 'img-item';
         author.className = 'author-tag';
         title.className = 'title-tag';
+        deleteButton.className = 'delete-button';
 
         author.textContent = data[i].author;
         title.textContent = data[i].title;
         image.src = data[i].path;
+        deleteButton.innerHTML = 'X';
 
         parent.appendChild(card);
+        card.appendChild(deleteButton);
         card.appendChild(author);
         card.appendChild(title);
         card.appendChild(image);
 
         document.querySelector('#input-tag').value = '';
-
-        image.onmouseover = 'bigImg(this)';
-        image.onmouseout = 'normalImg(this)';
-
-        function bigImg(x) {
-            x.style.height = "300px";
-            x.style.width = "200px";
-        }
-          
-        function normalImg(x) {
-            x.style.height = "270px";
-            x.style.width = "170px";
-        }
     }
 })
-
-// image.onmouseover = function(event) {
