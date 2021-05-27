e830638db6e5429fbac02f83aaca26b096333b6e
yeva <eva.hovnanyan.1999@gmail.com>
2021-05-27 16:28:22 +0400

    Added Book_Shop task.

---
+++ JS/Vahe/Book_Shop/src/index.js
@@ -0,0 +1,61 @@
+import _ from 'lodash';
+import './style.css';
+import books from './books.json';
+
+var list = document.createElement('div');
+list.className = 'list-books';
+
+
+function main() {
+  const formBlock = document.querySelector('#form-block')
+  
+  formBlock.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    let inputValue = document.querySelector('#input-block').value;  
+    if (0 < inputValue && inputValue <= books.length) {
+      removeBook();
+      setBooks(inputValue);
+    } else {
+      alert("Wrong input value.");
+    }
+  });
+}
+
+function setBooks(count) {  
+  for (let index = 0; index < count; index++) {
+     
+    const element = books[index];
+    
+    const title = document.createElement('div');
+    title.textContent = element.title;
+    title.className = 'title';
+    
+    const author = document.createElement('div');
+    author.textContent = element.author;
+    author.className = 'author';
+    
+
+    const image = document.createElement('img');
+    image.src = element.image;
+    image.className = 'image';
+    
+    const book = document.createElement('div');
+    book.className = 'book';
+    
+    book.appendChild(image);
+    book.appendChild(title);
+    book.appendChild(author);
+    list.appendChild(book);
+  }  
+  document.querySelector('body').appendChild(list);
+}
+
+function removeBook() {
+  if (list.childElementCount > 0) {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+  }
+}
+
+main();
---
+++ JS/Vahe/Book_Shop/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+          patterns: [
+            { from: "src/images", to: "images" },
+            { from: "src/index.html", to: "" },
+          ],
+        }),
+      ],
+
+
+};
