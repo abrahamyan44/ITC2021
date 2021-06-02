3d60533b2fc4bfb91c5c0eba81ffe91c9d1754f6
meryhovsepyan <44844776+meryhovsepyan@users.noreply.github.com>
2021-06-02 12:16:11 +0400

    Delete JS/Mery/ui-neumorphism-mery directory

--- JS/Mery/ui-neumorphism-mery/src/hocs/index.js
+++
@@ -1,3 +0,0 @@
-export { default as withImage } from './withImage.jsx'
-export { default as withResize } from './withResize.jsx'
-export { default as withClickOutside } from './withClickOutside.jsx'
