351860c0da3f2dad8713d46b487a3ec09eb38947
Sepuh Hovhannisyan <sepuh.hovhannisyan.01@mail.ru>
2021-06-03 10:43:53 +0400

    deleted HealthApp

--- JS/Sepuh/ui-neumorphismm/example/src/examples/index.js
+++
@@ -1,5 +0,0 @@
-export { default as FitnessApp } from './FitnessApp.jsx'
-export { default as Program } from './Program.jsx'
-export { default as ToDo } from './ToDo.jsx'
-export { default as Food } from './Food.jsx'
-export { default as Accaunt } from './Accaunt.jsx'
