4b18ece1219e79b94daf6801e0c5e85306e3b2d9
yeva <eva.hovnanyan.1999@gmail.com>
2021-06-05 20:01:50 +0430

    Added converting functionalities.

--- Web/Converter/converter.js
+++ Web/Converter/converter.js
@@ -1,57 +1,165 @@
 const selectId = document.querySelector("#select-id");
-const tables = document.querySelector(".tables")
-const objOfMass = {a1: "b1", a2: "b2", a3: "b3"};
-const objOfArea = {c1: 1, c2: 2, c3: 3};
+const tables = document.querySelector(".tables");
 
-selectId.addEventListener('change', function(evt){
-   const value = evt.target.value;
-   if (value === '1') {
-      CreateTables(objOfMass)
-   } else if (value === '2') {
-      CreateTables(objOfArea)
+const objOfMass = { Kilogram: "Gram", Tonne: "Kilogram", Gram: "Tonne" };
+const objOfArea = {
+  "Square Kilometre": "Square Mile",
+  "Square Meter": "Square Kilometre",
+  "Square Mile": "Square Meter",
+};
+const objofLength = {
+  Centimeter: "Meter",
+  Meter: "Milimetre",
+  Milimetre: "Centimeter",
+};
+
+selectId.addEventListener("change", function (evt) {
+  const value = evt.target.value;
+  if (value === "1") {
+    CreateTables(objOfMass);
+  } else if (value === "2") {
+    CreateTables(objOfArea);
+  } else if (value === "3") {
+    CreateTables(objofLength);
+  } else {
+    tables.innerHTML = "";
   }
-})
+});
 
-function CreateTables(obj){
-    tables.innerHTML=("");
-    const div1 = document.createElement("div");
-    const div2 = document.createElement("div");
-    //const icon = document.createElement("i");
+function CreateTables(obj) {
+  tables.innerHTML = "";
+  const div1 = document.createElement("div");
+  const div2 = document.createElement("div");
+  const select1 = document.createElement("select");
+  const select2 = document.createElement("select");
+  const input1 = document.createElement("input");
+  const input2 = document.createElement("input");
+  const swap = document.createElement("div");
 
-    div1.className = "div-1";
-    div2.className = "div-2";
-   // icon.className = "icon";
+  div1.className = "div-1";
+  div2.className = "div-2";
+  input1.className = "first-input";
+  input2.className = "second-input";
+  select1.className = "first-select";
+  select2.className = "second-select";
+  swap.className = "swap";
 
-    const select1 = document.createElement("select")
-    const select2 = document.createElement("select")
+  //initialisation
+  for (let key in obj) {
+    const option = document.createElement("option");
+    option.value = key;
+    option.text = key;
+    select1.appendChild(option);
+  }
 
-    const input1 = document.createElement("input")
-    const input2 = document.createElement("input")
+  Object.values(obj).forEach(function (val) {
+    const option = document.createElement("option");
+    option.value = val;
+    option.text = val;
+    select2.appendChild(option);
+  });
 
+  div1.appendChild(input1);
+  div2.appendChild(input2);
+  div1.appendChild(select1);
+  div2.appendChild(select2);
+  tables.appendChild(div1);
+  tables.appendChild(swap);
+  tables.appendChild(div2);
 
-    for (let key in obj) {
-        const option = document.createElement("option");
-        option.value = key;
-        option.text = key;
-        select1.appendChild(option);
+  Converting(input1, input2, select1, select2, swap);
+}
+
+function Converting(input1, input2, select1, select2, swap) {
+  input2.value = "";
+  input1.value = "";
+
+  input1.addEventListener("input", function () {
+    if (selectId.value === "1") {
+      convertingMass(input1, input2, select1, select2);
+    } else if (selectId.value === "2") {
+      //TODO
+      //convertingArea(input1, input2, select1, select2);
+    } else {
+      //TODO
+      //convertingLength(input1, input2, select1, select2);
     }
+  });
 
-    Object.values(obj).forEach(function(val){
-        const option = document.createElement("option");
-        option.value = val;
-        option.text = val;
-        select2.appendChild(option);
-    })
+  select1.addEventListener("change", function () {
+    Converting(input1, input2, select1, select2);
+  });
 
-    //var e = document.createElement("h5");
-    //e.append('<i class="fa fa-trash-o" aria-hidden="true"></i>');
-    //e.innerHTML = '<li>This<i class="fa fa-cross"></i></li>'
+  select2.addEventListener("change", function () {
+    Converting(input1, input2, select1, select2);
+  });
 
-    div1.appendChild(input1)
-    div2.appendChild(input2)
-    div1.appendChild(select1)
-    div2.appendChild(select2)
-    tables.appendChild(div1)
-    //tables.appendChild(e)
-    tables.appendChild(div2)
+  
+}
+
+function convertingMass(input1, input2, select1, select2) {
+  if (select1.value === "Kilogram" && select2.value === "Gram") {
+    input2.value = kgToG(input1.value);
+  } else if (select1.value === "Kilogram" && select2.value === "Tonne") {
+    input2.value = kgToTonne(input1.value);
+  } else if (select1.value === "Tonne" && select2.value === "Gram") {
+    input2.value = tonneToG(input1.value);
+  } else if (select1.value === "Tonne" && select2.value === "Kilogram") {
+    input2.value = tonneToKg(input1.value);
+  } else if (select1.value === "Gram" && select2.value === "Kilogram") {
+    input2.value = gToKg(input1.value);
+  } else if (select1.value === "Gram" && select2.value === "Tonne") {
+    input2.value = gToTonne(input1.value);
+  } else {
+    // kgToKg or gToG or tonneToTonne
+    input2.value = input1.value;
+  }
+}
+
+function kgToG(value) {
+  if (value > 0) {
+    return value * 1000;
+  } else {
+    return "";
+  }
+}
+
+function kgToTonne(value) {
+  if (value > 0) {
+    return value / 1000;
+  } else {
+    return "";
+  }
+}
+
+function tonneToG(value) {
+  if (value > 0) {
+    return value * 1000000;
+  } else {
+    return "";
+  }
+}
+
+function tonneToKg(value) {
+  if (value > 0) {
+    return value * 1000;
+  } else {
+    return "";
+  }
+}
+
+function gToKg(value) {
+  if (value > 0) {
+    return value / 1000;
+  } else {
+    return "";
+  }
+}
+
+function gToTonne(value) {
+  if (value > 0) {
+    return value / 1000000;
+  } else {
+    return "";
+  }
 }
