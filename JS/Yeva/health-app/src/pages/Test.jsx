4d91e2264229090c541c96f739c1869ebb40847b
yeva <eva.hovnanyan.1999@gmail.com>
2021-06-07 07:08:54 +0430

    Deleted Books Store, build files.

--- JS/Yeva/health-app/src/pages/Test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { TextArea } from 'ui-neumorphism'
-
-export class Abc extends React.Component {
-  render() {
-    return <TextArea autoExpand></TextArea>
-  }
-}
--- JS/Yeva/health-app/src/pages/Test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Dialog } from 'ui-neumorphism';
-
-export class Abc extends React.Component {
-  render() {
-    return <div><Dialog  ></Dialog></div>
-  }
-}
