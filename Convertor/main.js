270784bddbb53cc4cfb7b3c91e1a31f38bd07ef1
ElyaHakobyan <elya_hakobyan_2019@mail.ru>
2021-06-10 02:42:15 -0400

    dinamic adding items

--- Convertor/main.js
+++ Convertor/main.js
@@ -1,25 +1,35 @@
 const form = document.querySelector('.formList');
-const container = document.querySelector('.container');
+const container = document.querySelector('.selectMesure');
 const firstNum = document.querySelector('.firstNum');
 const secondNum = document.querySelector('.secondNum');
 const firstContainer = document.querySelector('.first-form-list');
 const secondContainer = document.querySelector('.second-form-list');
 
+let measurements = {
+    "select": {},
+    "mass": {
+        "kg": 1,
+        "g": 0.001,
+        "mg": 0.000001,
+        "t": 1000
+    },
+    "length": {
+        "m": 1,
+        "sm": 0.01,
+        "mm": 0.001,
+        "km": 1000
+    }
+}
 
-function CreateSelect(data, containerName, className, selectClassName, selected) {
+function CreateSelect(data, containerName, className, selectClassName) {
 
     let Select = document.createElement('select');
     Select.className = selectClassName;
 
     for(let i = 0; i < data.length; i++) { 
         let option1 = document.createElement('option');
-        option1.value = data[i].value;
-        option1.textContent = data[i].text;
+        option1.textContent = data[i];
         option1.className = className;
-
-        if((selected && option1.value == 'kilometer') || (selected && option1.value == 'kilogram')) {
-          option1.selected = true;
-        }
         Select.appendChild(option1);
         containerName.appendChild(Select);
     }
@@ -27,33 +37,18 @@ function CreateSelect(data, containerName, className, selectClassName, selected)
     return containerName;
 }
 
-const lengthArray = [
-    {
-        value: 'meter',
-        text: 'm',
-    },
-    {
-        value: 'kilometer',
-        text: 'km',
-    }
-] 
+let measureData = Object.keys(measurements);
+let Select = document.createElement('select');
+Select.className = 'select-mesure';
 
-const massArray = [
-    {
-        value: 'gram',
-        text: 'g'
-    },
-    {
-        value: 'kilogram',
-        text: 'kg'
-    }
-]
+for(let i = 0; i < measureData.length; i++) { 
+    let option1 = document.createElement('option');
+    option1.textContent = measureData[i];
+    Select.appendChild(option1);
+    container.appendChild(Select);
+}
 
 form.addEventListener('click', function(event) {
-    let val;
-    let firstVal;
-    let secondtVal;
-
     firstNum.value = '';
     secondNum.value = '';
     firstNum.disabled = false;
@@ -63,15 +58,10 @@ form.addEventListener('click', function(event) {
     firstContainer.innerHTML = '';
     secondContainer.innerHTML = '';
 
-    const el = document.querySelector('.selectMesure').value;
-    console.log(el);
-    if(el === 'length') {
-        CreateSelect(lengthArray, firstContainer, 'first-option', 'first-select');
-        CreateSelect(lengthArray, secondContainer, 'second-option', 'second-select', true);
-    } else if(el === 'mass') {
-        CreateSelect(massArray, firstContainer, 'first-option', 'first-select');
-        CreateSelect(massArray, secondContainer, 'second-option', 'second-select', true);
-    }
+    let measure = Select.value;
+    let data = Object.keys(measurements[measure]);
+    CreateSelect(data, firstContainer, 'first-option', 'first-select');
+    CreateSelect(data, secondContainer, 'second-option', 'second-select');
 
     firstContainer.addEventListener('click', function(event) {
         firstNum.value = '';
@@ -86,36 +76,25 @@ form.addEventListener('click', function(event) {
     })
 
     let switchButton = document.querySelector('.switch');
-    let a = document.querySelector('.first-select')
-    let opt1 = a.options[a.selectedIndex].text;
+    let a = document.querySelector('.first-select');
     let b = document.querySelector('.second-select')
-    let opt2 = b.options[b.selectedIndex].text;
 
     switchButton.addEventListener('click', function(event) {
         let temp = firstNum.value;
         firstNum.value = secondNum.value;
         secondNum .value = temp;
 
-        let tmp = opt1;
-        opt1.value = opt2;
-        opt2 = tmp;
+        let tmp = a.value;
+        a.value = b.value;
+        b.value = tmp;
     })
 
+    function Convert() {
+        let toVal = firstNum.value * measurements[measure][a.value] / measurements[measure][b.value];
+        secondNum.value = toVal;
+    }
+
     firstNum.addEventListener('input', function(event) {
-        if(firstVal === 'meter' && secondtVal === 'kilometer') {
-            val = event.target.value * 1000;
-        } else if(firstVal === 'kilometer' && secondtVal === 'meter') {
-            val = event.target.value / 1000;
-        } else if(firstVal === 'gram' && secondtVal === 'kilogram') {
-            val = event.target.value * 1000;
-        } else if(firstVal === 'kilogram' && secondtVal === 'gram') {
-            val = event.target.value / 1000;
-        } else  if (firstVal === secondtVal) {
-            val = event.target.value * 1000;
-        } else {
-            val = event.target.value;
-        }
-        
-        secondNum.value = val;
+        Convert();
     })
 })
