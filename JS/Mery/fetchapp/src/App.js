c5b4dbac1df5d9db5890e63830da450cdea3401e
Mery <mery.hovsepyan99@gmail.com>
2021-06-11 10:29:37 +0400

    added fetch app

---
+++ JS/Mery/fetchapp/src/App.js
@@ -0,0 +1,13 @@
+import List from '../src/List';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Col /> */}
+        <List  />
+    </div>
+  );
+}
+
+export default App;
