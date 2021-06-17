5470d3b0083d67dddb83bbd81f002712d08a4327
HakobyAnna <annnahakopyan@gmail.com>
2021-06-17 15:31:07 +0400

    making dynamic version

--- Web/Converter/src/index.js
+++ Web/Converter/src/index.js
@@ -1,44 +1,52 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-const measurements = require('./measurements.json');
+const measurements = require("./measurements.json");
 
-let input = document.getElementById('input');
-let result = document.getElementById('result');
-let inputType = document.getElementById('inputType');
-let resultType = document.getElementById('resultType');
-let measureType = document.getElementById('measureType')
+let input = document.getElementById("input");
+let result = document.getElementById("result");
+let inputType = document.getElementById("inputType");
+let resultType = document.getElementById("resultType");
+let measureType = document.getElementById("measureType");
 
-input.addEventListener('keyup', converter);
-inputType.addEventListener('change', converter);
-resultType.addEventListener('change', converter);
+input.addEventListener("keyup", converter);
+measureType.addEventListener("click", options_input_result);
+inputType.addEventListener("change", converter);
+resultType.addEventListener("change", converter);
 
-
-function options() {
-    let data = Object.keys(measurements)
-    for(let i = 0; i < data.length; i++) {
-        let options = document.createElement("option");
-        options.value = data[i];
-        options.text = data[i];
-        measureType.appendChild(options);
-    
-        let objName = data[i];
-       let arrayname = Object.keys(measurements[objName]);
-            for(let j = 0; j < arrayname.length; j++) {
-                let option1 = document.createElement("option");
-                option1.value = arrayname[j];
-                option1.text = arrayname[j];
-                inputType.appendChild(option1);
-                let option2 = document.createElement("option");
-                option2.value = arrayname[j];
-                option2.text = arrayname[j];
-                resultType.appendChild(option2);
-            }
-    }
+let data = Object.keys(measurements);
+function options_measureType() {
+  console.log("--- data: ", data);
+  for (let i = 0; i < data.length; i++) {
+    let options = document.createElement("option");
+    options.value = data[i];
+    options.text = data[i];
+    measureType.appendChild(options);
+  }
 }
 
-options();
+function options_input_result() {
+  console.log("begin......");
+  let object_name = measureType.value;
+  let data_input = Object.keys(measurements[object_name]);
+  console.log(data_input);
+  console.log(data_input.length);
+  for (let i = 0; i < data_input.length; i++) {
+    console.log(data_input[i]);
+    let option1 = document.createElement("option");
+    option1.value = data_input[i];
+    option1.text = data_input[i];
+    inputType.appendChild(option1);
+    let option2 = document.createElement("option");
+    option2.value = data_input[i];
+    option2.text = data_input[i];
+    resultType.appendChild(option2);
+  }
+}
+
+options_measureType();
 function converter() {
-    result.value = input.value * measurements[measureType.value][inputType.value] / measurements[measureType.value][resultType.value];
-    return result.value;
+  result.value =
+    (input.value * measurements[measureType.value][inputType.value]) /
+    measurements[measureType.value][resultType.value];
+  return result.value;
 }
-
